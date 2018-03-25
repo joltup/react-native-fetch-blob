d5f4d61321cc0136f070f74efa74bca84e6712c6
Egor Khmelev <hmelyoff@gmail.com>
2018-03-25 19:00:45 +0300

    Handle failure when download to FileStorage

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
@@ -511,11 +511,12 @@ public class RNFetchBlobReq extends BroadcastReceiver implements Runnable {
                     // It uses customized response body which is able to report download progress
                     // and write response data to destination path.
                     resp.body().bytes();
+
+                    this.destPath = this.destPath.replace("?append=true", "");
+                    callback.invoke(null, RNFetchBlobConst.RNFB_RESPONSE_PATH, this.destPath);
                 } catch (Exception ignored) {
-//                    ignored.printStackTrace();
+                    callback.invoke("RNFetchBlob failed.", null);
                 }
-                this.destPath = this.destPath.replace("?append=true", "");
-                callback.invoke(null, RNFetchBlobConst.RNFB_RESPONSE_PATH, this.destPath);
                 break;
             default:
                 try {
