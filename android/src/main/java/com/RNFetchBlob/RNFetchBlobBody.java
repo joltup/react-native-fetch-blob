d2f6f9305ed219b3d46dbfd595f996e4df2f8253
Elias Nahum <nahumhbl@gmail.com>
2018-03-31 13:27:16 +0300

    Fix multi part request

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobBody.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobBody.java
@@ -187,8 +187,8 @@ class RNFetchBlobBody extends RequestBody{
         ReactApplicationContext ctx = RNFetchBlob.RCTContext;
 
         for(FormField field : fields) {
-            String data = field.data;
-            String name = field.name;
+            String data = field.data.trim();
+            String name = field.name.trim();
             // skip invalid fields
             if(name == null || data == null)
                 continue;
@@ -230,14 +230,12 @@ class RNFetchBlobBody extends RequestBody{
                     byte[] b = Base64.decode(data, 0);
                     os.write(b);
                 }
-
             }
             // data field
             else {
-                header += "Content-Disposition: form-data; name=\"" + name + "\"\r\n";
-                header += "Content-Type: " + field.mime + "\r\n\r\n";
+                header += "Content-Disposition: form-data; name=\"" + name + "\";\r\n\r\n";
                 os.write(header.getBytes());
-                byte[] fieldData = field.data.getBytes();
+                byte[] fieldData = data.getBytes();
                 os.write(fieldData);
             }
             // form end
